0fb44187bd342a7fa323acddd0c6348c136fb622
Cloud User <ec2-user@ip-172-31-12-28.ap-south-1.compute.internal>
2023-09-01 16:54:42 +0530

    renamed service name

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-        name      : 'Ximkart API',
+        name      : 'Document Gen Service',
         script    : './bin/app.js',
         node_args : '-r dotenv/config'
     }],
