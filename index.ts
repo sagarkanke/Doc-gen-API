fc50ab3ae1d8e392db25c33b8e5ad038f5113818
digitalflakesagar <sagar@digitalfalke.com>
2023-10-18 13:15:49 +0530

    index.ts file removed

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-'use strict'
-const awsServerlessExpress = require('aws-serverless-express');
-import app  from "./app/config/express";
-const server = awsServerlessExpress.createServer(app);
-//@ts-ignore
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
+// 'use strict'
+// const awsServerlessExpress = require('aws-serverless-express');
+// import app  from "./app/config/express";
+// const server = awsServerlessExpress.createServer(app);
+// //@ts-ignore
+// exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
